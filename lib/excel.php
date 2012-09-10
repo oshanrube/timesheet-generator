cb76ca3b808f645694d05f4c7119bcaea9e23449
Oshan Rube <oshanrube@gmail.com>
2012-09-10 17:18:54 +0530

    with excel.php

---
+++ lib/excel.php
@@ -0,0 +1,113 @@
+<?php
+include('PHPExcel.php');
+require_once 'PHPExcel/Cell/AdvancedValueBinder.php';
+
+class Excel{
+	private $objPHPExcel;
+	private $initials = 'O.R.';
+	private $filename;
+	
+	public function __construct() {
+		/** Create a new PHPExcel Object  **/
+		$this->objPHPExcel = new PHPExcel();
+		$this->objPHPExcel->getProperties()
+		            ->setCreator("Maarten Balliauw")
+		            ->setLastModifiedBy("Maarten Balliauw")
+		            ->setTitle("Office 2007 XLSX Test Document")
+		            ->setSubject("Office 2007 XLSX Test Document")
+		            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+		            ->setKeywords("office 2007 openxml php")
+		            ->setCategory("Test result file");
+		$this->filename = 'timesheet-'.date("YmdHis").'.xlsx';
+	}
+	
+	public function createExcel($records) {
+		$this->createHeader();
+		$this->createBody($records);
+		$this->createOutput();
+	}
+	
+	private function createHeader() {
+		/*HEADER STYLING*/
+		$styleArray = array(
+			'font' => array('bold' => true,),
+			'alignment' => array('horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_RIGHT,),
+			'borders' => array('top' => array('style' => PHPExcel_Style_Border::BORDER_THIN,),),
+		);
+		$this->objPHPExcel->getActiveSheet()->getStyle('A1:H1')->applyFromArray($styleArray);
+		
+		/*HEADER*/
+		$this->objPHPExcel->setActiveSheetIndex(0);
+		$this->objPHPExcel->getActiveSheet()->setCellValue('A1', 'Project Name');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('B1', 'Start Date');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('C1', 'Time');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('D1', 'End Date');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('E1', 'Time');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('F1', 'Comment');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('G1', 'Initials');
+		$this->objPHPExcel->getActiveSheet()->setCellValue('H1', 'Spent');
+		/*SET COLUMN WIDTH*/
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('E')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('G')->setAutoSize(true);
+		$this->objPHPExcel->getActiveSheet()->getColumnDimension('H')->setAutoSize(true);
+		/*!HEADER*/
+		
+		/*SET COLUMN STYLING*/
+		$this->objPHPExcel->getActiveSheet()
+		            ->getStyle('B')
+		            ->getNumberFormat()
+		            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDDSLASH);
+		
+		$this->objPHPExcel->getActiveSheet()
+		            ->getStyle('C')
+		            ->getNumberFormat()
+		            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_TIME3);
+		            
+		$this->objPHPExcel->getActiveSheet()
+		            ->getStyle('D')
+		            ->getNumberFormat()
+		            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_DDMMYYYY);
+		
+		$this->objPHPExcel->getActiveSheet()
+		            ->getStyle('E')
+		            ->getNumberFormat()
+		            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_TIME3);
+		            
+		$this->objPHPExcel->getActiveSheet()
+		            ->getStyle('H')
+		            ->getNumberFormat()
+		            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_TIME3);
+	}
+	
+	private function createBody($records) {
+		/*BODY*/
+		$row = 3;
+		foreach($records as $record){
+			list($SDate,$STime) = explode(' ',$record->start_time);
+			list($EDate,$ETime) = explode(' ',$record->end_time);
+			
+			$this->objPHPExcel->getActiveSheet()->setCellValue('A'.$row, $record->projectname);			
+			PHPExcel_Cell::setValueBinder( new PHPExcel_Cell_AdvancedValueBinder() );
+			$this->objPHPExcel->getActiveSheet()->setCellValue('B'.$row, $SDate);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('C'.$row, $STime);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('D'.$row, $EDate);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('E'.$row, $ETime);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('F'.$row, $record->taskname);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('G'.$row, $this->initials);
+			$this->objPHPExcel->getActiveSheet()->setCellValue('H'.$row, '=E'.$row.'-C'.$row);
+			$row++;
+		}
+	}
+
+	private function createOutput() {
+		$objWriter = PHPExcel_IOFactory::createWriter($this->objPHPExcel, 'Excel2007');
+		header('Content-type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment; filename="'.$this->filename.'"');
+		$objWriter->save('php://output');
+	}
+}
---
+++ pages/export.php
@@ -0,0 +1,70 @@
+<?php
+require 'api.php';
+$api = new timesheetApi();
+?>
+<form action="server.php?action=export" method="POST">
+	<select name="project_id" id="project">
+		<option value="">All</option>
+		<?php foreach($api->getProjects() as $project):?>
+			<option value="project-<?php echo $project->id?>"><?php echo $project->name?></option>
+		<?php endforeach;?>
+	</select>
+	<select name="task_id" id="task">
+		<option value="">All</option>
+		<?php foreach($api->getTasks() as $task):?>
+			<option id="task-project-<?php echo $task->project_id?>" value="task-<?php echo $task->id?>"><?php echo $task->comment?></option>
+		<?php endforeach;?>
+	</select>
+	<select name="interval" id="interval">
+		<option value="">Custom</option>
+		<option value="day">Day</option>
+		<option value="week">Week</option>
+		<option value="month">Month</option>
+		<option value="year">Year</option>
+	</select>
+	<div class="input-append date datepicker" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+		<input name="start_time" id="start_time" type="text" value="<?php echo $data['start_time']?>"  placeholder="From: (YYYY-MM-DD HH:MM:SS)"/>
+	  <span class="add-on"><i class="icon-th"></i></span>
+	</div>
+	
+	<div class="input-append date datepicker" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+		<input name="end_time" id="end_time" type="text" value="<?php echo $data['end_time']?>"  placeholder="To: (YYYY-MM-DD HH:MM:SS)"/>
+		<span class="add-on"><i class="icon-th"></i></span>
+	</div>
+	<button class="btn btn-success"> Export </button>
+</form>
+<script type="text/javascript">
+	$(document).ready(function(){
+		//var options = { ¨¨
+		//	clearForm: true,
+		//	success:    function() { loadStatus(); loadPage(); } 
+		//};
+		//$('form#export').ajaxForm(options);
+		$('#project form').change(function(){
+			var project_id = $(this).val();
+			$('form #task option').show();
+			if(project_id != ''){
+				$('form #task option').each(function(){
+					var id = $(this).attr('id');
+					var pattern = '/task-'+project_id+'/';
+					pattern.replace('-','\-');
+					if( id && !(pattern.match(id))){						
+						$(this).hide();
+					}
+				})
+			}
+		});
+		$('.datepicker').datepicker();
+		$('#interval').change(function(){
+			var interval = $(this).val();
+			if(interval != ''){
+				$('form #start_time').attr("disabled", "disabled");
+				$('form #end_time').attr("disabled", "disabled");
+			} else {
+				$('form #start_time').removeAttr("disabled");
+				$('form #end_time').removeAttr("disabled");
+			}
+		});
+	});
+	
+</script>

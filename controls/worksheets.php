acb9e365877379ed6fc3d2f317299460d0984757
Oshan Rube <oshanrube@gmail.com>
2012-09-14 08:48:51 +0530

    with the form for adding custom worklogs

--- controls/worksheets.php
+++ controls/worksheets.php
@@ -2,14 +2,23 @@
 require_once 'api.php';
 $api = new timesheetApi();
 ?>
-<form class="form-inline" method="post" id="interval" action="server.php?action=interval">
-<?php if($reason = $api->getInterval()){ ?>
-<label style="margin: 10px 10px 10px 0;height: 16px;padding: 6px;"  class="control-label"><?php echo $reason?></label>
-<button name="action" value="resume" class="btn btn-info pull-right">Resume</button>
-<?php } else { ?>
-<input style="width: 583px;" type="text" name="reason" placeholder="Reason: I gotta go.... Do something...." />
-<button name="action" value="pause" class="btn btn-primary pull-right">Pause</button>
-<?php } ?>
+<form class="form-inline" method="post" id="customTask" action="server.php?action=customTask">
+<select name="project_id" id="project">
+	<?php foreach($api->getProjects() as $project):?>
+		<option value="project-<?php echo $project->id?>"><?php echo $project->name?></option>
+	<?php endforeach;?>
+</select>
+<input type="text" name="comment" placeholder="Comment: " />
+<div class="input-append date datepicker" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+	<input name="start_time" id="start_time" type="text" value="<?php echo $data['start_time']?>"  placeholder="From: (YYYY-MM-DD HH:MM:SS)"/>
+  <span class="add-on"><i class="icon-th"></i></span>
+</div>
+
+<div class="input-append date datepicker" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+	<input name="end_time" id="end_time" type="text" value="<?php echo $data['end_time']?>"  placeholder="To: (YYYY-MM-DD HH:MM:SS)"/>
+	<span class="add-on"><i class="icon-th"></i></span>
+</div>
+<button name="action" value="pause" class="btn btn-primary pull-right">Add</button>
 </form>
 <script>
 $(document).ready(function(){
@@ -18,5 +27,6 @@ $(document).ready(function(){
 		success:    function() { loadStatus(); loadPage(); } 
 	};
 	$('form#interval').ajaxForm(options);
+	$('.datepicker').datepicker();
 });
 </script>
